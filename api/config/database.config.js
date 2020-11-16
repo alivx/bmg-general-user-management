2d811ae98f5f8fdaeaee945f6fd9b51e81ba7cf9
Ali Saleh Baker <alivxlive@gmail.com>
2020-11-16 22:28:06 +0200

    Fix config files

--- api/config/database.config.js
+++ api/config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb://127.0.0.1:27017/bmg",
+    url: "mongodb://localhost:27017/bmg?retryWrites=true&w=majorityaleh",
 };
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    apiUrl: 'http://127.0.0.1:3001/api/users'
+    apiUrl: 'http://localhost:3001/api/users'
 }
